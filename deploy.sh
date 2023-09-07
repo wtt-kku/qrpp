5ecd27b6e4773846ff2bcc321b7c5cac3c74d4b1
Withan WTT <withan@kkumail.com>
2023-09-08 05:50:05 +0700

    Update deploy.sh

--- deploy.sh
+++ deploy.sh
@@ -1,10 +1,1 @@
-DIR="./.next"
-if [ -d "$DIR" ]; then
-  # Take action if $DIR exists. #
-  echo "Installing config files in ${DIR}..."
-  rm -rf ./.next
-fi
-
-npm run build
-
-pm2 --name "QR-API" start server.js
+pm2 --name "QR-API" start index.js
